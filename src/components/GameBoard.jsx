28023dc41d6461a464cd6970a2336ea5a4ca2694
Luigi Matias <luigi.matias@codehunter.com>
2024-01-23 18:07:41 -0500

    sharing states across components

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -11,20 +11,8 @@ export default function GameBoard({ onSelectSquare, turns }) {
     const { square, player } = turn;
     const { row, col } = square;
 
-    gameBoard[row][col] = player; // this line was failing
+    gameBoard[row][col] = player;
   }
-
-  // const [gameBoard, setGameBoard] = useState(initialGameBoard);
-
-  // function handleSelectSquare(rowIndex, colIndex) {
-  //   setGameBoard((prevGameBoard) => {
-  //     const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
-  //     updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-  //     return updatedBoard;
-  //   });
-
-  //   onSelectSquare();
-  // }
 
   return (
     <ol id="game-board">
