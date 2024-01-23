6ac4e36749a871fc722290c43c556cf26664fb8f
Luigi Matias <luigi.matias@codehunter.com>
2024-01-23 13:46:43 -0500

    lifting state up

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-const Player = ({ initialName, symbol }) => {
+const Player = ({ initialName, symbol, isActive }) => {
   const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
 
@@ -31,7 +31,7 @@ const Player = ({ initialName, symbol }) => {
   };
 
   return (
-    <li>
+    <li className={isActive ? "active" : undefined}>
       <span className="player">
         <EditablePlayerName />
         <span className="player-symbol">{symbol}</span>
